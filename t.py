ae1c41d3e8d01aacdab421292839af149d9b4ceb
Satyamashu05 <Satyam.Dewangan@iiitb.ac.in>
2026-05-01 07:38:55 +0000

    Removed t.py

--- t.py
+++
@@ -1,8 +0,0 @@
-from host.context import Context
-from host.runtime import Runtime
-
-rt = Runtime()
-
-ctx = rt.load_module("plugins/build/example_c.wasm")
-print(rt.sum( 1, 2))
-print(rt.noReturn(ctx))
